511096b6d4e73a20dbf3589b2143f5c8fbafd774
Luan-art <luanlf2003@gmail.com>
2024-04-19 22:43:26 -0300

    Add project files.

---
+++ imc/Program.cs
@@ -0,0 +1,72 @@
+﻿string genero;
+float peso = 0, altura = 0, IMC = 0;
+
+do
+{
+    Console.WriteLine("Iforme seu peso em quilos: ");
+    peso = float.Parse(Console.ReadLine());
+
+} while (peso <= 0);
+
+do
+{
+    Console.WriteLine("Iforme sua altura em Metros: ");
+    altura = float.Parse(Console.ReadLine());
+
+} while (altura <= 0);
+
+do
+{
+    Console.WriteLine("Digite seu gênero [F/M]:");
+    genero = Console.ReadLine();
+
+} while (genero != "F" && genero != "M");
+
+IMC = peso / (altura * altura);
+
+if (IMC < 17)
+{
+    Console.WriteLine("Muito abaixo do peso");
+}
+else
+{
+    if (IMC < 18.5)
+    {
+        Console.WriteLine("Abaixo do peso");
+    }
+    else
+    {
+        if (IMC < 24.9)
+        {
+            Console.WriteLine("Peso Normal");
+        }
+        else
+        {
+            if (IMC < 29.9)
+            {
+                Console.WriteLine("Acima do peso");
+            }
+            else
+            {
+                if(IMC < 34.3)
+                {
+                    Console.WriteLine("Obesidade | ");
+                }
+                else
+                {
+                    if(IMC < 39.9)
+                    {
+                        Console.WriteLine("Obesidade ||");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Obesidade |||");
+
+                    }
+                }
+            }
+        }
+    }
+    
+}
